7e52494ea865a80e485e2c0968d106ed542c7528
Sam <sam@codewizardshq.com>
2022-12-16 14:33:16 -0500

    invoke uvicorn programmatically as a workaround

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World!"}
+    return {"Hello": "World@"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    import sys
+    import uvicorn
+
+    debugging = "pydevd" in sys.modules
+    uvicorn.run("main:app", reload=not debugging, host="0.0.0.0", port=8080)
